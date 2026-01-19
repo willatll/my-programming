1f1dda3ba605e0ce2a82a3fb3561187113ee3b2b
willatll <zhaowilliam07@gmail.com>
2026-01-19 15:12:42 -0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,15 @@
+
+
+def whatever():
+    print("Hello World")
+
+def input_output():
+    name=input("Enter your name:")
+    age=int(input("Enter your age:"))     
+    height=float(input("Enter your height:(in meters)"))  
+
+    print(f"Hello {name}")
+    print(f"You are{age} years old") 
+    print(f"your height is {height}meters") 
+
+input_output()
